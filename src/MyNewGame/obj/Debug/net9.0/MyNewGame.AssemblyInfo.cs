cd8d7527fdf55b20d1fddd0fb270c727130618e6
ภูมิพัฒน์ ตามวงค์ <poomipat3456@gmail.com>
2026-06-23 15:51:26 +0700

    Add native libraries and project files for MyNewGame targeting .NET 9.0
    - Added OpenAL and SDL2 native libraries for various platforms (Android, iOS, Linux, macOS, Windows).
    - Included necessary assembly info and project files for .NET 9.0.
    - Generated project.assets.json and related cache files for dependency management.
    - Updated project configuration to support MonoGame framework and its dependencies.

--- src/MyNewGame/obj/Debug/net9.0/MyNewGame.AssemblyInfo.cs
+++ src/MyNewGame/obj/Debug/net9.0/MyNewGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyNewGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d985a685b8e59c4478110fe16873036af0b662d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyNewGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyNewGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
